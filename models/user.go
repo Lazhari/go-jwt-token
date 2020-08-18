909f2d4aff11997202ebc2f0a036229b3b1a3ab6
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2020-08-19 00:01:26 +0200

    Update the user model

--- models/user.go
+++ models/user.go
@@ -1,11 +1,30 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jinzhu/gorm"
+)
 
 // User model
 type User struct {
-	gorm.Model
-	ID       int    `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ID        string     `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time  `json:"createdAt"`
+	UpdatedAt time.Time  `json:"updatedAt"`
+	DeletedAt *time.Time `json:"-"`
+	FirstName string     `json:"firstName"`
+	LastName  string     `json:"lastName"`
+	Phone     string     `json:"phone"`
+	IsActive  bool       `gorm:"DEFAULT:false" json:"isActive"`
+	JobTitle  string     `json:"jobTitle"`
+	Company   string     `json:"company"`
+	Email     string     `gorm:"unique;not null" json:"email"`
+	Password  string     `json:"password"`
+}
+
+// BeforeCreate hook runs before creating the user
+func (u *User) BeforeCreate(scope *gorm.Scope) error {
+	scope.SetColumn("ID", uuid.New().String())
+	return nil
 }
