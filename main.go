c465c68f9ce53a189b4f15cb36244cc3256a49e4
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2020-08-20 00:32:38 +0200

    Refactore the app routing

--- main.go
+++ main.go
@@ -5,15 +5,12 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	"github.com/lazhari/web-jwt/controllers"
-
-	"github.com/lazhari/web-jwt/middleware"
-
 	"github.com/lazhari/web-jwt/driver"
+	"github.com/lazhari/web-jwt/middleware"
 	"github.com/subosito/gotenv"
-
-	"github.com/gorilla/mux"
 )
 
 func init() {
@@ -22,14 +19,40 @@ func init() {
 
 var db *gorm.DB
 
+// Route represent the application routes
+type Route struct {
+	path    string
+	handler http.HandlerFunc
+	method  string
+}
+
 func main() {
 	db = driver.ConnectDB()
 	controller := controllers.Controller{}
 	r := mux.NewRouter()
 	r.Use(middleware.CommonMiddleware)
-	r.HandleFunc("/sign-up", controller.SignUpHandler(db)).Methods("POST")
-	r.HandleFunc("/login", controller.LoginHandler(db)).Methods("POST")
-	r.HandleFunc("/protected", middleware.IsAuthenticated(controller.ProtectedHandler())).Methods("GET")
+
+	routes := []Route{
+		{
+			path:    "/sign-up",
+			handler: controller.SignUpHandler(db),
+			method:  "POST",
+		},
+		{
+			path:    "/login",
+			handler: controller.LoginHandler(db),
+			method:  "POST",
+		},
+		{
+			path:    "/protected",
+			handler: middleware.IsAuthenticated(controller.ProtectedHandler()),
+			method:  "GET",
+		},
+	}
+
+	for _, route := range routes {
+		r.HandleFunc(route.path, route.handler).Methods(route.method)
+	}
 
 	fmt.Println("Server running on port 8080...")
 	log.Fatal(http.ListenAndServe(":8050", r))
