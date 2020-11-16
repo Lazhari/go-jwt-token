01c250738a2014a864bdb8022808756dde1e0128
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2020-11-16 20:15:58 +0100

    Create the post handler and implete the create

--- api/post.go
+++ api/post.go
@@ -1,71 +1,48 @@
 package api
 
-// CreatePost creates a new post
-/* func (c Controller) CreatePost(db *gorm.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		userID := middleware.GetUserID(ctx)
-		post := models.Post{}
-		err := &models.RequestError{}
+import (
+	"encoding/json"
+	"net/http"
 
-		json.NewDecoder(r.Body).Decode(&post)
-		post.AuthorID = userID
+	"github.com/lazhari/web-jwt/middleware"
+	"github.com/lazhari/web-jwt/models"
+	"github.com/lazhari/web-jwt/post"
+	"github.com/lazhari/web-jwt/utils"
+)
 
-		errV := post.Validate()
+// PostHandler the post hander interface
+type PostHandler interface {
+	CreatePost(http.ResponseWriter, *http.Request)
+	GetAllPosts(http.ResponseWriter, *http.Request)
+}
 
-		if errV != nil {
-			utils.RespondWithError(w, &models.RequestError{
-				StatusCode:       http.StatusBadRequest,
-				Message:          "Invalid request",
-				ValidationErrors: errV,
-			})
-			return
-		}
+type postHandler struct {
+	postService post.Service
+}
 
-		dbc := db.Create(&post)
+// NewPostHandler creates a new post handler
+func NewPostHandler(postSrv post.Service) PostHandler {
+	return &postHandler{postService: postSrv}
+}
 
-		if dbc.Error != nil {
-			err.Message = "Internal server error"
-			err.StatusCode = http.StatusInternalServerError
+func (p *postHandler) CreatePost(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	userID := middleware.GetUserID(ctx)
+	post := &models.Post{}
 
-			utils.RespondWithError(w, err)
-		}
+	json.NewDecoder(r.Body).Decode(post)
+	post.AuthorID = userID
 
-		utils.ResponseJSON(w, post)
-		return
+	post, err := p.postService.Create(post)
+
+	if err != nil {
+		utils.RespondWithError(w, err)
 	}
-} */
 
-// GetPostByID retrieve a post by ID
-/* func (c Controller) GetPostByID(db *gorm.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		err := &models.RequestError{}
-		vars := mux.Vars(r)
+	utils.ResponseJSON(w, post)
+	return
+}
 
-		id := vars["id"]
-		if id == "" {
-			err.Message = "ID is required"
-			err.StatusCode = http.StatusBadRequest
+func (p *postHandler) GetAllPosts(w http.ResponseWriter, r *http.Request) {
 
-			utils.RespondWithError(w, err)
-		}
-
-		post := models.Post{}
-		dbc := db.First(&post, "id = ?", id)
-
-		if dbc.Error != nil {
-			if dbc.Error == sql.ErrNoRows {
-				err.Message = "The user does not exist!"
-				err.StatusCode = http.StatusNotFound
-			} else {
-				err.Message = dbc.Error.Error()
-				err.StatusCode = http.StatusInternalServerError
-			}
-			utils.RespondWithError(w, err)
-			return
-		}
-
-		utils.ResponseJSON(w, post)
-		return
-	}
-} */
+}
--- post/post.go
+++ post/post.go
@@ -1,6 +1,10 @@
 package post
 
-import "github.com/lazhari/web-jwt/models"
+import (
+	"net/http"
+
+	"github.com/lazhari/web-jwt/models"
+)
 
 type postService struct {
 	postRepo Repository
@@ -14,9 +18,29 @@ func NewPostService(postRepo Repository) Service {
 }
 
 func (postSrv *postService) Create(p *models.Post) (*models.Post, error) {
-	return postSrv.postRepo.Create(p)
+	err := &models.RequestError{}
+
+	validationErr := p.Validate()
+
+	if validationErr != nil {
+		return nil, &models.RequestError{
+			StatusCode:       http.StatusBadRequest,
+			Message:          "Invalid request",
+			ValidationErrors: validationErr,
+		}
+	}
+
+	p, createError := postSrv.postRepo.CreatePost(p)
+	if createError != nil {
+		err.Message = "Internal server error"
+		err.StatusCode = http.StatusInternalServerError
+
+		return nil, err
+	}
+
+	return p, nil
 }
 
 func (postSrv *postService) GetAll() ([]models.Post, error) {
-	return postSrv.postRepo.GetAll()
+	return postSrv.postRepo.GetAllPosts()
 }
